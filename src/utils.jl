55e8fae4fadffbf4a99343f7a21203f862bccc71
kura-okubo <kura.okubo@gmail.com>
2019-06-14 20:48:06 -0400

    Update utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -111,7 +111,8 @@ function testdownload(InputDict::Dict{String,Any}, numofitr::Int64, MAX_MEM_PER_
 
         while true
             global t1 = @elapsed global Stest = seisdownload_NOISE(trial_id, InputDict) #[s]
-            if Stest[1].misc["dlerror"] == 0
+            dl = [Stest[i].misc["dlerror"] for i in 1:size(Stest)[1]]
+            if issubset(0, dl)
                 break;
             else
                 trial_id += 1
@@ -122,7 +123,8 @@ function testdownload(InputDict::Dict{String,Any}, numofitr::Int64, MAX_MEM_PER_
 
         while true
             global t1 = @elapsed global Stest = seisdownload_EARTHQUAKE(trial_id, InputDict) #[s]
-            if Stest[1].misc["dlerror"] == 0
+            dl = [Stest[i].misc["dlerror"] for i in 1:size(Stest)[1]]
+            if issubset(0, dl)
                 break;
             else
                 trial_id += 1
@@ -156,6 +158,7 @@ function testdownload(InputDict::Dict{String,Any}, numofitr::Int64, MAX_MEM_PER_
     println(@sprintf("Total download size will be %4.2f [%s].", 0.8 * totaldownloadsize, sizeunit)) #0.6: considering compression efficiency
     println(@sprintf("Download will finish at %s.", round(estimated_downloadtime, Dates.Second(1))))
     println("*We have a time lag with downloading time above, like in 10 minutes or so.*")
+    println("*This estimation also changes if some download requests fail and are skipped.*")
     println("-------START DOWNLOADING-------------")
 
     return max_num_of_processes_per_parallelcycle
