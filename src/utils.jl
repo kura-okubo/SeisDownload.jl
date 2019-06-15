71b100e61215cfee2b7f25fe2c1de2944c857f26
kura-okubo <kura.okubo@gmail.com>
2019-06-14 21:06:37 -0400

    Update utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -111,10 +111,7 @@ function testdownload(InputDict::Dict{String,Any}, numofitr::Int64, MAX_MEM_PER_
 
         while true
             global t1 = @elapsed global Stest = seisdownload_NOISE(trial_id, InputDict) #[s]
-            dl = [Stest[i].misc["dlerror"] for i in 1:size(Stest)[1]]
-            println(dl)
-            println(issubset(0, dl))
-            
+            dl = [Stest[i].misc["dlerror"] for i in 1:size(Stest)[1]]            
             if issubset(0, dl)
                 break;
             else
