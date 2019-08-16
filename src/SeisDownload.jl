dbb9fc7786a79433e4fa41e5893c4715790b6171
kura-okubo <kurama_okubo@fas.harvard.edu>
2019-08-15 21:24:12 -0400

    update towards v1.2

--- src/SeisDownload.jl
+++ src/SeisDownload.jl
@@ -13,26 +13,28 @@ using SeisIO, Dates, Printf, JLD2, FileIO, Distributed
 #For the time being, we need remove_response function from obspy
 #This will be replaced by SeisIO modules in the near future.
 #Please activate obspy enviroment before launching Julia.
-include("remove_response_obspy.jl")
-using .Remove_response_obspy
+# include("remove_response_obspy.jl")
+# using .Remove_response_obspy
 #------------------------------------------------------------------#
 
 export seisdownload
 
-
 """
-    seisdownload(InputDict::Dict; MAX_MEM_PER_CPU::Float64=1.0)
+    seisdownload(InputDict::Dict)
 
     Request seismic data and save into jld2.
 # Arguments
 - `InputDict`    : dictionary which contains request information
-- `MAX_MEM_PER_CPU` : maximum available memory for 1 cpu (unit:GB) (default = 1.0GB)
 """
-function seisdownload(InputDict::Dict; MAX_MEM_PER_CPU::Float64=1.0)
+function seisdownload(InputDict::Dict)
 
     Utils.initlogo()
 
+	printparams(InputDict)
+
 	DownloadType    = InputDict["DownloadType"]
+
+	mkpath("./seisdownload_tmp")
 
     if DownloadType == "Noise" || DownloadType == "noise"
 
@@ -45,7 +47,7 @@ function seisdownload(InputDict::Dict; MAX_MEM_PER_CPU::Float64=1.0)
 		fopath          = InputDict["fopath"]
 
 		if mod((endtime - starttime).value,  DL_time_unit) != 0 || (endtime - starttime).value < DL_time_unit
-			error("Total download time cannot be devided by Download Time unit; this may cause unexpected result. abort.")
+			error("Total download time cannot be devided by Download Time unit; this may cause unexpected result. Abort.")
 		end
 
 		# calculate start time list (starttimelist) with each Donwload_time_unit
@@ -56,98 +58,28 @@ function seisdownload(InputDict::Dict; MAX_MEM_PER_CPU::Float64=1.0)
 		InputDict["starttimelist"] = starttimelist
 		InputDict["DLtimestamplist"] = DLtimestamplist
 
-		#save info into jld2
-		jldopen(fopath, "w") do file
-			file["info/DLtimestamplist"] = DLtimestamplist;
-			file["info/stationlist"] = stationlist;
-			file["info/starttime"]   = string(starttime)
-			file["info/endtime"]     = string(endtime)
-			file["info/DL_time_unit"]= string(DL_time_unit)
+		#----Restrict number of processors------#
+		#NEVER CHANGE THIS THRESHOLD OTHERWISE IT OVERLOADS THE DATA SERVER
+		np = Sys.CPU_THREADS
+		if np > 32 throw(DomainError(np, "np must be smaller than 32.")) end
+		#---------------------------------------#
+
+        # Test download to evaluate use of memory and estimate download time.
+		testdownload(InputDict, length(starttimelist))
+
+		# Start downloading data
+		t_download = @elapsed pmap(x -> seisdownload_NOISE(x, InputDict), 1:length(starttimelist))
+
+		# convert intermediate file to prescibed file format (JLD2, ASDF, ...)
+		t_convert = @elapsed convert_tmpfile(InputDict)
+
+		println("---Summary of computational time---")
+		println(@sprintf("Total download time:%8.4f[s]", t_download))
+		println(@sprintf("Total convert time:%8.4f[s]", t_convert))
+
+		if !InputDict["Istmpfilepreserved"]
+			rm("./seisdownload_tmp", recursive=true, force=true)
 		end
-
-        #Test download to evaluate use of memory and estimate download time.
-		max_num_of_processes_per_parallelcycle = testdownload(InputDict, length(starttimelist), MAX_MEM_PER_CPU)
-
-        if max_num_of_processes_per_parallelcycle < 1
-            error("Memory allocation is not enought (currently $MAX_MEM_PER_CPU [GB]). Please inclease MAX_MEM_PER_CPU or decrease number of stations")
-        end
-
-        if max_num_of_processes_per_parallelcycle >= length(starttimelist)
-
-            S = pmap(x -> seisdownload_NOISE(x, InputDict), 1:length(starttimelist))
-
-            # save data to jld2
-            file = jldopen(fopath, "r+")
-            unavalilablefile = jldopen(join([fopath[1:end-5], "_unavailablestations.jld2"]), "w+")
-
-            for ii = 1:size(S)[1] #loop at each starttime
-                for jj = 1:size(S[1])[1] #loop at each station id
-
-                    requeststr =S[ii][jj].id
-                    varname = joinpath(DLtimestamplist[ii], requeststr)
-                    #save_SeisData2JLD2(fopath, varname, S[ii][jj])
-                    file[varname] = S[ii][jj]
-
-                    if S[ii][jj].misc["dlerror"] == 1
-                        unavalilablefile[varname] = S[ii][jj]
-                    end
-                end
-            end
-            JLD2.close(file)
-            JLD2.close(unavalilablefile)
-
-        else
-
-            #parallelization by time
-    	    pitr = 1
-
-    	    # progress bar
-
-    	    while pitr <= length(starttimelist)
-
-    	        startid1 = pitr
-    	        startid2 = pitr + max_num_of_processes_per_parallelcycle - 1
-
-    	        if startid1 == length(starttimelist)
-					S = pmap(x -> seisdownload_NOISE(x, InputDict), startid1:startid1)
-
-				elseif startid2 <= length(starttimelist)
-    	            #use all processors
-	                S = pmap(x -> seisdownload_NOISE(x, InputDict), startid1:startid2)
-
-    	        else
-    	            #use part of processors
-	                startid2 = startid1 + mod(length(starttimelist), max_num_of_processes_per_parallelcycle) - 1
-	                S = pmap(x -> seisdownload_NOISE(x, InputDict), startid1:startid2)
-    	        end
-
-                # save data to jld2
-                file = jldopen(fopath, "r+")
-                unavalilablefile = jldopen(join([fopath[1:end-5], "_unavailablestations.jld2"]), "w+")
-
-                for ii = 1:size(S)[1] #loop at each starttime
-                    for jj = 1:size(S[1])[1] #loop at each station id
-
-                        requeststr =S[ii][jj].id
-                        varname = joinpath(DLtimestamplist[startid1+ii-1], requeststr)
-                        #save_SeisData2JLD2(fopath, varname, S[ii][jj])
-                        file[varname] = S[ii][jj]
-
-                        if S[ii][jj].misc["dlerror"] == 1
-                            unavalilablefile[varname] = S[ii][jj]
-                        end
-                    end
-                end
-
-                JLD2.close(file)
-                JLD2.close(unavalilablefile)
-
-
-    	        pitr += max_num_of_processes_per_parallelcycle
-
-                #println("pitr: $pitr")
-            end
-        end
 
     elseif  DownloadType == "Earthquake" || DownloadType == "earthquake"
 
